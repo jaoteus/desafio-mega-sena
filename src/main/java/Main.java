ad3b932497c417e1ca3b842a653d89767ea40382
Mateus <mateusff2323@gmail.com>
2024-03-08 14:35:30 -0300

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,103 +1,53 @@
 import java.util.Random;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.lang.StringBuilder;
-import java.util.Scanner;
 
 public class Main {
-    private static StringBuilder combinacao = new StringBuilder();
-    private static final String numeros = "0123456789";
-    private static StringBuilder primeiraDezena =  new StringBuilder();
-    private static StringBuilder segundaDezena =  new StringBuilder();
-    private static StringBuilder terceiraDezena =  new StringBuilder();
-    private static StringBuilder quartaDezena =  new StringBuilder();
-    private static StringBuilder quintaDezena =  new StringBuilder();
-    private static StringBuilder sextaDezena =  new StringBuilder();
-    private static ArrayList<String> combinacoesSorteadas = new ArrayList<String>();
-    private static char numeroAleatorio;
-    private static final int quantidadeNumerosParaSortear = 12;
-    private static int quantidadeNumerosSorteados = 0;
-    private static int quantidadeDeCombinacoesDesejadas = 1;
-    private static int quantidadeDeCombinacoesFeitas = 0;
-    private static boolean usarPadrao = false;
-    private static final Random random = new Random();
-    private static final Scanner scanner = new Scanner(System.in);
-
-    private static final int numeroMaximo = 60;
+    private static StringBuilder dezena = new StringBuilder();
+    private static char caracterAleatorio;
+    private static Random random = new Random();
+    //    private static Array
+    private static final String caracteres = "0123456789";
+    private static String primeiraDezena;
+    private static String segundaDezena;
+    private static String terceiraDezena;
+    private static String quartaDezena;
+    private static String quintaDezena;
+    private static String sextaDezena;
 
     public static void main(String[] args) {
-        setPrimeiraDezena();
-        setSegundaDezena();
+        primeiraDezena = setDezena();
+        segundaDezena = setDezena();
+        terceiraDezena = setDezena();
+        quartaDezena = setDezena();
+        quintaDezena = setDezena();
+        sextaDezena = setDezena();
 
-        if (segundaDezena.equals(primeiraDezena)){
-            segundaDezena.delete(0, segundaDezena.length());
-            setSegundaDezena();
-        }
-        setTerceiraDezena();
-        if (terceiraDezena.equals(segundaDezena)){
-            terceiraDezena.delete(0, terceiraDezena.length());
-            setTerceiraDezena();
-        }
-        setQuartaDezena();
-        if (quartaDezena.equals(terceiraDezena)){
-            quartaDezena.delete(0, quartaDezena.length());
-            setQuartaDezena();
-        }
-        setQuintaDezena();
-        if (quintaDezena.equals(quartaDezena)){
-            quintaDezena.delete(0, quintaDezena.length());
-            setQuintaDezena();
-        }
-        setSextaDezena();
-        if (sextaDezena.equals(quintaDezena)){
-            sextaDezena.delete(0, sextaDezena.length());
-            setSextaDezena();
-        }
-
-        combinacao.append(primeiraDezena);
-        combinacao.append(segundaDezena);
-        combinacao.append(terceiraDezena);
-        combinacao.append(quartaDezena);
-        combinacao.append(quintaDezena);
-        combinacao.append(sextaDezena);
-        System.out.println(combinacao);
+        System.out.printf("%s - %s - %s - %s - %s - %s",
+                primeiraDezena, segundaDezena, terceiraDezena, quartaDezena, quintaDezena, sextaDezena);
     }
 
-    public static void setPrimeiraDezena() {
-        while (primeiraDezena.length() < 2) {
-            numeroAleatorio = numeros.charAt(random.nextInt(numeros.length()));
-            primeiraDezena.append(numeroAleatorio);
+    public static String setDezena() {
+        dezena.delete(0, dezena.length());
+        while (dezena.toString().length() < 2) {
+            if (dezena.toString().startsWith("6") ||
+                    dezena.toString().startsWith("7") ||
+                    dezena.toString().startsWith("8") ||
+                    dezena.toString().startsWith("9")
+            ) {
+                dezena.delete(0, dezena.length());
+                continue;
+            } else if (dezena.toString().equals("00") ||
+                    dezena.toString().equals("11") ||
+                    dezena.toString().equals("22") ||
+                    dezena.toString().equals("33") ||
+                    dezena.toString().equals("44") ||
+                    dezena.toString().equals("55")
+            ) {
+                dezena.delete(0, dezena.length());
+                continue;
+            }
+            caracterAleatorio = caracteres.charAt(random.nextInt(caracteres.length()));
+            dezena.append(caracterAleatorio);
         }
-    }
-
-    public  static  void setSegundaDezena(){
-        while (segundaDezena.length() < 2) {
-            numeroAleatorio = numeros.charAt(random.nextInt(numeros.length()));
-            segundaDezena.append(numeroAleatorio);
-        }
-    }
-    public  static  void setTerceiraDezena(){
-        while (terceiraDezena.length() < 2) {
-            numeroAleatorio = numeros.charAt(random.nextInt(numeros.length()));
-            terceiraDezena.append(numeroAleatorio);
-        }
-    }
-    public  static  void setQuartaDezena(){
-        while (quartaDezena.length() < 2) {
-            numeroAleatorio = numeros.charAt(random.nextInt(numeros.length()));
-            quartaDezena.append(numeroAleatorio);
-        }
-    }
-    public  static  void setQuintaDezena(){
-        while (quintaDezena.length() < 2) {
-            numeroAleatorio = numeros.charAt(random.nextInt(numeros.length()));
-            quintaDezena.append(numeroAleatorio);
-        }
-    }
-    public  static  void setSextaDezena(){
-        while (sextaDezena.length() < 2) {
-            numeroAleatorio = numeros.charAt(random.nextInt(numeros.length()));
-            sextaDezena.append(numeroAleatorio);
-        }
+        return dezena.toString();
     }
 }
